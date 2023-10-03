61cb16b3ee9c0c43e939471b094e28b78da6043e
Filippo Chinni Carella <144950309+filippochinni@users.noreply.github.com>
2023-10-03 21:30:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,7 @@
-//create a simple web server that can return a list of comments and add comments
-//to the list of comments
+//create a web server
 var express = require('express');
-var bodyParser = require('body-parser');
-var comments = require('./comments.json');
-var app = express();
-
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-
-//routes
-app.get('/comments', function(req, res){
-  res.send({comments: comments});
-});
-
-app.post('/comments', function(req, res){
-  var comment = req.body;
-  comments.push(comment);
-  res.send(comments);
-});
-
-app.listen(3000, function(){
-  console.log('server listening on port 3000');
-});
+var router = express.Router();
+var Comment = require('../models/comment');
+var jwt = require('jsonwebtoken');
+var config = require('../config');
+var User = require('../models/user');
